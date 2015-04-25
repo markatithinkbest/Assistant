95b537e86852c4541ff1712b128d263789e90dd0
Mark Chen <mark@ithinkbest.com>
2015-04-25 18:13:56 +0800

    going to keep answers

--- app/src/main/java/com/ithinkbest/phoneix/assistant/S1Activity.java
+++ app/src/main/java/com/ithinkbest/phoneix/assistant/S1Activity.java
@@ -6,29 +6,67 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
+
+import java.util.Arrays;
 
 
 public class S1Activity extends ActionBarActivity {
     static String LOG_TAG = "MARK987";
-    static int questionNumber=1;
+    static int questionNumber = 0;
+    static int totalQuestion = 3;
+
 
     TextView txtQuestion;
     RadioButton radioBtn1;
     RadioButton radioBtn2;
     RadioButton radioBtn3;
+    Button btnPrev;
+    Button btnNext;
 
+
+    static final String[] QUESTION = new String[]{
+            "000",
+            "111您使用Anroid手機,已經多久了?",
+
+            "您使用Anroid手機,已經多久了?",
+            "3. 請您自我評估使用Anroid手機的熟練度?"
+
+    };
+    //    <string name="s1_01">1. 操作這支APP您覺得?</string>
+//    <string name="s1_01a">容易</string>
+//    <string name="s1_01b">有點困難</string>
+//    <string name="s1_01c">很困難</string>
+//    radioBtn1.setText("三個月以內");
+//    radioBtn2.setText("三個月到一年左右");
+//    radioBtn3.setText("一年以上");
+//
+//    radioBtn1.setText("還很陌生");
+//    radioBtn2.setText("一般夠用");
+//    radioBtn3.setText("非常熟練");
+    static final String[][] QUESTION_SET = new String[][]{
+
+            {"1. 操作這支APP,您個人覺得?", "容易", "有點困難", "很困難"},
+            {"2. 您使用Anroid手機,已經多久了?", "三個月以內", "三個月到一年左右", "一年以上"},
+            {"3. 請您自我評估,使用Anroid手機的熟練度?", "還很陌生", "一般夠用", "非常熟練"}
+
+    };
+    static int[] answer={-1,-1,-1};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_s1);
-        txtQuestion=(TextView)findViewById(R.id.txtQuestion) ;
-        radioBtn1 =(RadioButton)findViewById(R.id.radioBtn1);
-        radioBtn2 =(RadioButton)findViewById(R.id.radioBtn2);
-        radioBtn3 =(RadioButton)findViewById(R.id.radioBtn3);
+        txtQuestion = (TextView) findViewById(R.id.txtQuestion);
+        radioBtn1 = (RadioButton) findViewById(R.id.radioBtn1);
+        radioBtn2 = (RadioButton) findViewById(R.id.radioBtn2);
+        radioBtn3 = (RadioButton) findViewById(R.id.radioBtn3);
+        btnPrev = (Button) findViewById(R.id.btnPrev);
+        btnNext = (Button) findViewById(R.id.btnNext);
 
+        ShowQuestion();
     }
 
 
@@ -54,59 +92,87 @@ public class S1Activity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void handleButtonsEnable() {
+        if (questionNumber == 0) {
+            btnPrev.setEnabled(false);
+        } else {
+            btnPrev.setEnabled(true);
+        }
+
+        if (questionNumber == totalQuestion - 1) {
+            btnNext.setEnabled(false);
+        } else {
+            btnNext.setEnabled(true);
+        }
+    }
+
     public void onPrevButtonClicked(View view) {
         questionNumber--;
+
         ShowQuestion();
 
     }
+
     public void onNextButtonClicked(View view) {
         questionNumber++;
+
         ShowQuestion();
     }
 
-    private void ShowQuestion(){
-        switch (questionNumber){
+    private void ShowQuestion() {
+        handleButtonsEnable();
+        txtQuestion.setText(QUESTION_SET[questionNumber][0]);
+        radioBtn1.setText(QUESTION_SET[questionNumber][1]);
+        radioBtn2.setText(QUESTION_SET[questionNumber][2]);
+        radioBtn3.setText(QUESTION_SET[questionNumber][3]);
+
+        switch (questionNumber) {
             case 2:
-                txtQuestion.setText("2. 您使用Anroid手機,已經多久了?");
-                radioBtn1.setText("三個月以內");
-                radioBtn2.setText("三個月到一年左右");
-                radioBtn3.setText("一年以上");
+//                txtQuestion.setText(QUESTION[2]);
+//                radioBtn1.setText("三個月以內");
+//                radioBtn2.setText("三個月到一年左右");
+//                radioBtn3.setText("一年以上");
                 break;
             case 3:
-                txtQuestion.setText("3. 請您自我評估使用Anroid手機的熟練度?");
-                radioBtn1.setText("還很陌生");
-                radioBtn2.setText("一般夠用");
-                radioBtn3.setText("非常熟練");
+//                txtQuestion.setText(QUESTION[3]);
+//                radioBtn1.setText("還很陌生");
+//                radioBtn2.setText("一般夠用");
+//                radioBtn3.setText("非常熟練");
                 break;
 
         }
     }
-    
+
 
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
 
         // Check which radio button was clicked
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.radioBtn1:
-                if (checked){
-                    Log.d(LOG_TAG,"...radioButton1");
+                if (checked) {
+                    answer[questionNumber]=1;
+                    Log.d(LOG_TAG, "...radioButton1 "+ Arrays.toString(answer));
+
                 }
-                    // Pirates are the best
-                    break;
+                // Pirates are the best
+                break;
             case R.id.radioBtn2:
-                if (checked){
-                    Log.d(LOG_TAG,"...radioButton2");
+                if (checked) {
+                    answer[questionNumber]=2;
+                    Log.d(LOG_TAG, "...radioButton2 "+ Arrays.toString(answer));
+
                 }
-                    // Ninjas rule
-                    break;
+                // Ninjas rule
+                break;
             case R.id.radioBtn3:
-                if (checked){
-                    Log.d(LOG_TAG,"...radioButton3");
+                if (checked) {
+                    answer[questionNumber]=3;
+                    Log.d(LOG_TAG, "...radioButton3 "+ Arrays.toString(answer));
                 }
-                    // Ninjas rule
-                    break;
+                // Ninjas rule
+                break;
         }
     }
 }
