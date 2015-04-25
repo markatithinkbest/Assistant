acc641baadd40ee8d6e5c9cb86c3551821c00f06
Mark Chen <mark@ithinkbest.com>
2015-04-25 14:16:13 +0800

    android:layout_height="wrap_content" fixed extra space row at

--- app/src/main/java/com/ithinkbest/phoneix/assistant/CmpRpt1Activity.java
+++ app/src/main/java/com/ithinkbest/phoneix/assistant/CmpRpt1Activity.java
@@ -1,43 +1,77 @@
 package com.ithinkbest.phoneix.assistant;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
 
 
 public class CmpRpt1Activity extends ActionBarActivity {
-
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cmp_rpt1);
+        setupWebView();
 
+//         webView = (WebView) findViewById(R.id.webView);
+//        webView.setHorizontalScrollBarEnabled(true);
+//        webView.setVerticalScrollBarEnabled(true);
+//        webView.getSettings().setUseWideViewPort(true);
+//        //Finally this one worked, show Chinese properly!
+//        webView.loadData(getHtml(), "text/html; charset=utf-8", "UTF-8");
+    }
+    String getHtml(){
         StringBuilder result=new StringBuilder();
-        result.append("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy111111234567566576876878798");
+        //  result.append("<head><meta name='viewport' content='target-densityDpi=device-dpi'/></head>");
+        String s="<head><meta name=viewport content=target-density dpi=medium-dpi, width=device-width/></head>";
+        result.append(s);
+        result.append("中文是必要的xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy111111234567566576876878798");
         result.append("<table border='1' cellpadding=\"0\" cellspacing=\"0\"");
-        for (int row=0;row<22;row++){
+        for (int row=0;row<52;row++){
             result.append("<tr>");
             for (int col=0;col<22;col++){
-                result.append("<td>"+col+"yyyyy </td>");
-
-
+                result.append("<td>&nbsp;"+col+"yyyyy&nbsp;</td>");
             }
             result.append("</tr>");
 
         }
         result.append("</table>");
+        result.append("中文是必要的xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy111111234567566576876878798");
+        return result.toString();
 
-        WebView webView = (WebView) findViewById(R.id.webView);
+    }
+    private void setupWebView() {
+
+        webView = (WebView) findViewById(R.id.webView);
         webView.setHorizontalScrollBarEnabled(true);
         webView.setVerticalScrollBarEnabled(true);
         webView.getSettings().setUseWideViewPort(true);
         //Finally this one worked, show Chinese properly!
-        webView.loadData(result.toString(), "text/html; charset=utf-8", "UTF-8");
+        webView.loadData(getHtml(), "text/html; charset=utf-8", "UTF-8");
     }
 
-
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.setWebViewClient(new WebViewClient() {
+//            @Override
+//            public void onPageFinished(WebView view, String url) {
+//                webView.loadUrl("javascript:MyApp.resize(document.body.getBoundingClientRect().height)");
+//                super.onPageFinished(view, url);
+//            }
+//        });
+//        webView.addJavascriptInterface(this, "MyApp");
+//    }
+//
+//    @JavascriptInterface
+//    public void resize(final float height) {
+//        CmpRpt1Activity.this.runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                webView.setLayoutParams(new LinearLayout.LayoutParams(getResources().getDisplayMetrics().widthPixels, (int) (height * getResources().getDisplayMetrics().density)));
+//            }
+//        });
+//    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
