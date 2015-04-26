b4e7766a990700ed1bc2700676dab0ed64c166d7
Mark Chen <mark@ithinkbest.com>
2015-04-27 07:03:09 +0800

    adding CheckSurveySumary

---
+++ app/src/main/java/com/ithinkbest/phoneix/assistant/CheckSurveySumaryActivity.java
@@ -0,0 +1,60 @@
+package com.ithinkbest.phoneix.assistant;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class CheckSurveySumaryActivity extends ActionBarActivity {
+    static String LOG_TAG = "MARK987 SurveyActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_survey, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickBtn1(View view) {
+//        Log.d(LOG_TAG, "...btn1");
+        Intent intent = new Intent(this, S1Activity.class);
+        startActivity(intent);
+
+
+
+    }
+    public void onClickBtn2(View view) {
+//        Log.d(LOG_TAG,"...btn2");
+
+
+
+    }
+    public void onClickBtn3(View view) {
+//        Log.d(LOG_TAG,"...btn3");
+
+    }
+}
